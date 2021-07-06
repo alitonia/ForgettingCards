066c03bae64387e3314b9a20f5921d7881bc2b2d
Zen-net <allyofjustice1@gmail.com>
2021-07-06 23:39:51 +0700

    initialize

--- android/app/src/main/java/com/forgettingcards/MainApplication.java
+++ android/app/src/main/java/com/forgettingcards/MainApplication.java
@@ -10,6 +10,8 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.forgettingcards.CustomMMKVJSIModulePackage; // <- add here
+import com.facebook.react.bridge.JSIModulePackage; // <-- ADD THIS
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -32,6 +34,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+          // add this method to load our CustomMMKVJSIModulePackage.
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+           return new CustomMMKVJSIModulePackage();
         }
       };
 
